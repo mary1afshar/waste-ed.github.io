3a1a833b96eb983da4b524b634a2d984ebd93123
zoede <zoede@users.noreply.github.com>
2021-01-10 00:08:42 -0500

    working collision

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
+let score = 0;
+
 function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
     var seconds = Math.floor((t / 1000) % 60);
-    
+
     return {
-      'total': t,
-      
-      'seconds': seconds
+        'total': t,
+
+        'seconds': seconds
     };
-  }
-  
-  /* timer */
-  function initializeClock(id, endtime) {
+}
+
+/* timer */
+function initializeClock(id, endtime) {
     var clock = document.getElementById(id);
     var secondsSpan = clock.querySelector('.seconds');
-  
+
     function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
-      if (t.total <= -1) {
-        clearInterval(timeinterval);
-        alert('counter ended!');
-      }
+        var t = getTimeRemaining(endtime);
+
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= -1) {
+            clearInterval(timeinterval);
+            alert('counter ended!');
+        }
     }
-  
+
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
-  }
-  
-  var deadline = new Date(Date.parse(new Date()) +  120 * 1000);
+}
 
-  initializeClock('clockdiv', deadline);
+var deadline = new Date(Date.parse(new Date()) + 120 * 1000);
+
+initializeClock('clockdiv', deadline);
 
 /*game*/
 const canvas = document.getElementById('canvas1');
@@ -64,12 +66,12 @@ background.src = "./images/background.png";
 // const playerSprite = new Image();
 // playerSprite.src = "unknown.png";
 
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 
-setInterval(function(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     // ctx.drawSprite(playerSprite, 0, 0, player.width, player.height, 0, 0);
     drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
@@ -78,30 +80,30 @@ setInterval(function(){
 }, 50);
 
 // Keyboard listeners
-window.addEventListener("keydown", function(e){
+window.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
 });
 
-window.addEventListener("keyup", function(e){
+window.addEventListener("keyup", function (e) {
     delete keys[e.keyCode];
     player.moving = false;
 });
 
-function movePlayer(){
-    if(keys[37] && player.x > 0){
+function movePlayer() {
+    if (keys[37] && player.x > 0) {
         player.x -= player.speed;
         player.frameY = 1
         player.moving = true;
     }
-    if(keys[39] && player.x < canvas.width - player.width){
+    if (keys[39] && player.x < canvas.width - player.width) {
         player.x += player.speed;
         player.frameY = 0
         player.moving = true;
     }
 }
 
-function handlePlayerFrame(){
-    if(player.frameX < 3 && player.moving) player.frameX++
+function handlePlayerFrame() {
+    if (player.frameX < 3 && player.moving) player.frameX++
     else player.frameX = 0;
 }
 
@@ -120,43 +122,52 @@ const banana = new Image();
 banana.src = "./images/fall_objects/object_banana.png";
 
 let objectX = [], objectY = [], objectDy = []; // Dy is change in y
-let object = [bottle, bag, can, messy_can, banana, battery]; 
+let object = [bottle, bag, can, messy_can, banana, battery];
 
 // Call before objects fall
-function startGame () {
-  for(let l = 0; l < 6; l++){
-    objectY[l] = -100;
-    objectX[l] = Math.random() * canvas.width-60;
-    objectDy[l] = 1.5 + Math.random() * 5;
-  }
+function startGame() {
+    for (let l = 0; l < 6; l++) {
+        objectY[l] = -100;
+        objectX[l] = Math.random() * canvas.width - 60;
+        objectDy[l] = 1.5 + Math.random() * 5;
+    }
 }
 
 // Initialize object specs
 startGame();
 
 // Loop objects falling
-setInterval(function(){
+setInterval(function () {
 
     // initialze (x, y) and falling speed for two recyclables
     // the recyclables
-    for (let i=0; i < 6; i++){
-    if(objectY[i] > canvas.height){
-        objectY[i] = -100;
-        objectX[i] = 30 + Math.random() * canvas.width-60;
-        objectDy[i] = 2 + Math.random() * 5;
-    } else { // or else just move down the object by the Dy
-        objectY[i] += objectDy[i];
+    for (let i = 0; i < 6; i++) {
+        if (objectY[i] > canvas.height) {
+            objectY[i] = -100;
+            objectX[i] = 30 + Math.random() * canvas.width - 60;
+            objectDy[i] = 2 + Math.random() * 5;
+        } else { // or else just move down the object by the Dy
+            objectY[i] += objectDy[i];
+        }
+        if (turtleHit(objectX[i], objectY[i], 40, 60)) {
+            objectY[i] = canvas.height + 100;
+            if(i < 3) {
+                score += 5;
+            } else {
+                score -= 10;
+            }
+            console.log(score);
+        }
+
+        ctx.drawImage(object[i], objectX[i], objectY[i]);
     }
 
-    ctx.drawImage(object[i], objectX[i], objectY[i]);
-  }
-    
 }, 50);
 
 function turtleHit(nX, nY, nW, nH) {
-    if (nX >= player.x && nX <= (player.x + 100) && (nY + nH) >= player.y && nY <= player.y) {
-      return true;
+    if (nX >= player.x && nX <= (player.x + 96) && (nY + nH) >= player.y && nY <= player.y) {
+        return true;
     } else {
-      return false;
+        return false;
     }
-  }
+}
