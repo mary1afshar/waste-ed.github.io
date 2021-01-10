7a9307a097b5e5315cbd3a8d5cf69552f06e2fb7
Maryam Afshar <59989874+mary1afshar@users.noreply.github.com>
2021-01-10 00:26:45 -0500

    update score

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
   }
   
-  var deadline = new Date(Date.parse(new Date()) +  30 * 1000);
+  var deadline = new Date(Date.parse(new Date()) +  45 * 1000);
 
 initializeClock('clockdiv', deadline);
 
@@ -157,6 +157,7 @@ setInterval(function () {
                 score -= 10;
             }
             console.log(score);
+            document.getElementById("score").innerHTML = score;
         }
 
         ctx.drawImage(object[i], objectX[i], objectY[i]);
@@ -173,4 +174,3 @@ function turtleHit(nX, nY, nW, nH) {
   }
 
  
-
